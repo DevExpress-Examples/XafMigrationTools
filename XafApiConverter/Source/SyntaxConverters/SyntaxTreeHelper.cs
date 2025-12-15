81e1f331c8ae5c40b4a8871df0d8d88aff965766
Maksim Karpenko <maksim.karpenko@devexpress.com>
2025-12-15 16:46:45 +0300

    Implemented add comment to commented lines

--- XafApiConverter/Source/SyntaxConverters/SyntaxTreeHelper.cs
+++ XafApiConverter/Source/SyntaxConverters/SyntaxTreeHelper.cs
@@ -4,8 +4,13 @@ using Microsoft.CodeAnalysis.CSharp;
 
 namespace XafApiConverter {
     static class SyntaxTreeHelper {
-        public static SyntaxNode CommentLine(SyntaxNode line) {
+        public static SyntaxNode CommentLine(SyntaxNode line, string addComment = null) {
             var leadingTrivias = line.GetLeadingTrivia();
+            if (!string.IsNullOrEmpty(addComment)) {
+                leadingTrivias = leadingTrivias.Add(SyntaxFactory.Comment($"// {addComment}"));
+                leadingTrivias = leadingTrivias.Add(SyntaxFactory.CarriageReturnLineFeed);
+                leadingTrivias = leadingTrivias.AddRange(line.GetLeadingTrivia());
+            }
             leadingTrivias = leadingTrivias.Add(SyntaxFactory.Comment("// "));
             return line.WithLeadingTrivia(leadingTrivias);
         }
