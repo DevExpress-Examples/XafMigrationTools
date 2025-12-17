f23b1fe116c0254355c22f8a6392f64b9099b794
Dmitrii Minakov <dmitry.minakov@devexpress.com>
2025-12-17 22:16:47 +0100

    add test case

---
+++ XafApiConverter/XafApiConverter.TestProject.Etalon/CustomLayoutTemplates.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Web.UI;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Blazor.Layout;
+using DevExpress.Web;
+
+namespace FeatureCenter.Module.Web.Layout {
+    // NOTE: Class commented out due to types having no XAF .NET equivalent
+    //   - Base class 'LayoutItemTemplate' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     LayoutItemTemplate has no Blazor equivalent (Web Forms layout specific)
+    //   - Base class 'LayoutItemTemplateContainer' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     LayoutItemTemplateContainer has no equivalent in XAF .NET (loaded from removed-api.txt)
+    // TODO: It is necessary to test the application's behavior and, if necessary, develop a new solution.
+    // ========== COMMENTED OUT CLASS ==========
+    // public class CustomLayoutItemTemplate : LayoutItemTemplate {
+    //         protected override Control CreateCaptionControl(LayoutItemTemplateContainer layoutItemTemplateContainer) {
+    //             Table table = new Table();
+    //             table.Rows.Add(new TableRow());
+    //             table.Rows[0].Cells.Add(new TableCell());
+    //             table.Rows[0].Cells.Add(new TableCell());
+    //             Control baseControl = base.CreateCaptionControl(layoutItemTemplateContainer);
+    //             table.Rows[0].Cells[0].Controls.Add(baseControl);
+    //             ASPxHyperLink anchor = new ASPxHyperLink();
+    //             anchor.Text = "?";
+    //             anchor.Style.Add(HtmlTextWriterStyle.FontWeight, "bold");
+    //             anchor.Style.Add(HtmlTextWriterStyle.TextDecoration, "underline");
+    //             anchor.NavigateUrl = "javascript:void(0);";
+    //             anchor.ToolTip = string.Format("Description for the '{0}' item", layoutItemTemplateContainer.ViewItem.Caption);
+    //             table.Rows[0].Cells[1].Controls.Add(anchor);
+    //             return table;
+    //         }
+    //     }
+    // ========================================
+
+    // NOTE: Class commented out due to types having no XAF .NET equivalent
+    //   - Base class 'LayoutGroupTemplate' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     LayoutGroupTemplate has no Blazor equivalent (Web Forms layout specific)
+    //   - Base class 'LayoutItemTemplateContainerBase' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     LayoutItemTemplateContainerBase has no equivalent in XAF .NET (loaded from removed-api.txt)
+    //   - Base class 'LayoutGroupTemplateContainer' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     LayoutGroupTemplateContainer has no equivalent in XAF .NET (loaded from removed-api.txt)
+    //   - Base class 'ASPxImageHelper' has no equivalent (inferred from using DevExpress.ExpressApp.Web)
+    //     ASPxImageHelper has no equivalent in XAF .NET (loaded from removed-api.txt)
+    // TODO: It is necessary to test the application's behavior and, if necessary, develop a new solution.
+    // ========== COMMENTED OUT CLASS ==========
+    // public class CustomLayoutGroupTemplate : LayoutGroupTemplate {
+    //         private static void AddControls(ControlCollection controlCollection, IEnumerable<Control> controlsToLayout) {
+    //             foreach(Control control in controlsToLayout) {
+    //                 LayoutItemTemplateContainerBase templateContainer = control as LayoutItemTemplateContainerBase;
+    //                 if(templateContainer != null && templateContainer.LayoutManager.DelayedItemsInitialization) {
+    //                     templateContainer.Instantiate();
+    //                 }
+    //                 controlCollection.Add(control);
+    //             }
+    //         }
+    //         protected override void LayoutContentControls(LayoutGroupTemplateContainer layoutGroupTemplateContainer, IList<Control> controlsToLayout) {
+    //             if(layoutGroupTemplateContainer.ShowCaption) {
+    //                 Panel panel = new Panel();
+    //                 panel.Style.Add(HtmlTextWriterStyle.Padding, "10px 5px 10px 5px");
+    //                 ASPxRoundPanel roundPanel = new ASPxRoundPanel();
+    //                 roundPanel.Width = Unit.Percentage(100);
+    //                 roundPanel.HeaderText = layoutGroupTemplateContainer.Caption;
+    //                 if(layoutGroupTemplateContainer.HasHeaderImage) {
+    //                     ASPxImageHelper.SetImageProperties(roundPanel.HeaderImage, layoutGroupTemplateContainer.HeaderImageInfo);
+    //                 }
+    //                 AddControls(roundPanel.Controls, controlsToLayout);
+    //                 panel.Controls.Add(roundPanel);
+    //                 layoutGroupTemplateContainer.Controls.Add(panel);
+    //             }
+    //             else {
+    //                 AddControls(layoutGroupTemplateContainer.Controls, controlsToLayout);
+    //             }
+    //         }
+    //     }
+    // ========================================
+
+    // NOTE: Class commented out due to types having no XAF .NET equivalent
+    //   - Base class 'TabbedGroupTemplate' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     TabbedGroupTemplate has no Blazor equivalent (Web Forms layout specific)
+    //   - Base class 'TabbedGroupTemplateContainer' has no equivalent (inferred from using DevExpress.ExpressApp.Web.Layout)
+    //     TabbedGroupTemplateContainer has no equivalent in XAF .NET (loaded from removed-api.txt)
+    // TODO: It is necessary to test the application's behavior and, if necessary, develop a new solution.
+    // ========== COMMENTED OUT CLASS ==========
+    // public class CustomLayoutTabbedGroupTemplate : TabbedGroupTemplate {
+    //         protected override ASPxPageControl CreatePageControl(TabbedGroupTemplateContainer tabbedGroupTemplateContainer) {
+    //             ASPxPageControl pageControl = base.CreatePageControl(tabbedGroupTemplateContainer);
+    //             pageControl.TabPosition = TabPosition.Left;
+    //             pageControl.ContentStyle.Paddings.Padding = Unit.Pixel(10);
+    //             return pageControl;
+    //         }
+    //     }
+    // ========================================
+
+}
