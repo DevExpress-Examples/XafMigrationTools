d87ed1ad0e5f3fa406c6da25f6e71aac794c5536
Maksim Karpenko <maksim.karpenko@devexpress.com>
2025-12-18 17:41:07 +0300

    SemanticCache - refactoring

---
+++ XafApiConverter/Source/Converter/SemanticCache.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XafApiConverter.Converter {
+    class SemanticCache {
+        readonly Dictionary<string, Item> _cache = new Dictionary<string, Item>();
+
+        public void Add(string fileName, SemanticModel semanticModel, SyntaxTree syntaxTree, Document document) {
+            _cache[fileName] = new Item(semanticModel, syntaxTree, document);
+        }
+
+        public Item TryGetValue(string fileName) {
+            return _cache.GetValueOrDefault(fileName);
+        }
+
+        public class Item {
+            public readonly SemanticModel SemanticModel;
+            public readonly SyntaxTree SyntaxTree;
+            public readonly Microsoft.CodeAnalysis.Document Document;
+            public Item(SemanticModel semanticModel, SyntaxTree syntaxTree, Document document) {
+                SemanticModel = semanticModel;
+                SyntaxTree = syntaxTree;
+                Document = document;
+            }
+        }
+    }
+}
